a27af0e321c6dcf2dba236b0b1154300bb753556
Bill <billmike1994@gmail.com>
2017-10-28 19:28:43 +0100

    Add API for user sign in

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,9 @@
+import jwt from 'jsonwebtoken';
 import { User } from '../models';
 import signupValidator from '../validators/validatesignup';
+import signinValidator from '../validators/validatesignin';
+
+const secret = 'secretkeyishere';
 
 class Users {
   static signUp(req, res, next) {
@@ -22,6 +26,26 @@ class Users {
         err.status = 400;
         return next(err);
       });
+  }
+
+  static signIn(req, res, next) {
+    const validateSignin = signinValidator(req.body);
+    if (validateSignin.valid) {
+      const { email, password } = req.body;
+      User.authenticate(email, password, (err, user) => {
+        if (err === undefined || !user || err) {
+          const err2 = new Error('Invalid email or password.');
+          err2.status = 401;
+          next(err2);
+        }
+        const token = jwt.sign({ id: user.id }, secret, { expiresIn: '60 days' });
+        return res.status(201).send({ status: 'Success', token: { token } });
+      });
+    } else {
+      const err = new Error(validateSignin.message);
+      err.status = validateSignin.status;
+      next(err);
+    }
   }
 }
 
---
+++ server/validators/validatesignin.js
@@ -0,0 +1,7 @@
+const validatesignin = ({ email, password }) => {
+  if (email === undefined || email === '') return { valid: false, message: 'Email is required.', status: 400 };
+  if (password === undefined || password === '') return { valid: false, message: 'Password is required.', status: 400 };
+  return { valid: true };
+};
+
+export default validatesignin;
