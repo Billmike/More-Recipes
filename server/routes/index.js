421c9a9ddae3d7188cbbe490d3fea26c04d7dc94
Bill <billmike1994@gmail.com>
2017-10-31 15:34:08 +0100

    Add API that enables users get their favorite recipes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ import User from '../middleware/isuser';
 import recipeAdd from '../middleware/validateAddRecipe';
 import reviewController from '../controllers/reviews';
 import validateReview from '../middleware/validateReview';
+import favorite from '../controllers/addFavorites';
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
@@ -17,4 +18,6 @@ module.exports = (app) => {
   app.get('/api/v1/recipes', recipeController.getRecipes);
   app.post('/api/v1/recipes/:recipeId', Login.ensureLogin, User.isuser, validateReview, reviewController.addReviews);
   app.delete('/api/v1/recipes/:recipeId', Login.ensureLogin, User.isuser, recipeController.deleteRecipe);
+  app.post('/api/v1/recipes/:recipeId/favorites', Login.ensureLogin, User.isuser, favorite.addFavorite);
+  app.get('/api/v1/users/:userId/recipes', Login.ensureLogin, User.isuser, favorite.getFavorites);
 };
