851cf0c6edb73b18dac37ffebb5b09c0e5237310
Bill <billmike1994@gmail.com>
2017-11-02 12:26:51 +0100

    Refactor filre for hosting heroku

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import http from 'http';
 
 const app = express();
 
@@ -11,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('json spaces', 4);
-const port = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 app.set('port', port);
 
 require('./routes')(app);
@@ -20,7 +19,7 @@ app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of aawesomeness',
 }));
 
-const server = http.createServer(app);
-server.listen(port);
+
+app.listen(port);
 
 module.exports = app;
