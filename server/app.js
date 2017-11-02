e5243a380b8cf1408cc02fe6b1cced12f4c217da
Bill <billmike1994@gmail.com>
2017-11-02 11:43:31 +0100

    Properly deploy on Heroku

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import http from 'http';
 
 const app = express();
 
@@ -10,11 +11,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('json spaces', 4);
+const port = parseInt(process.env.PORT, 10) || 5000;
+app.set('port', port);
 
 require('./routes')(app);
 
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of aawesomeness',
 }));
+
+const server = http.createServer(app);
+server.listen(port);
 
 module.exports = app;
