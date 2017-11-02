df4c31d4d75bf20b33aef724ceee3e8e0794a1ee
Bill <billmike1994@gmail.com>
2017-11-02 14:43:29 +0100

    Configure config file to Config.js properly

--- server/app.js
+++ server/app.js
@@ -10,16 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('json spaces', 4);
-const port = process.env.PORT || 5000;
-app.set('port', port);
 
 require('./routes')(app);
 
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of aawesomeness',
 }));
-
-
-app.listen(port);
 
 module.exports = app;
