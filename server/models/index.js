990cfd094d08f10d00f2cd5d9138e0c0e461ec21
Bill <billmike1994@gmail.com>
2017-11-02 14:15:46 +0100

    Configure config file properly

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,7 @@ import Sequelize from 'sequelize';
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 console.log(process.env.NODE_ENV);
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
