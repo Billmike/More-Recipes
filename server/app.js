11eda8162f9b9a9b961042a505fbaf0e7f2c288a
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-8.local>
2017-12-07 08:45:45 +0100

    Rectify issue with homepage in deployment

--- server/app.js
+++ server/app.js
@@ -8,14 +8,13 @@ require('dotenv').config();
 
 const app = express();
 const appPath = path.join(__dirname, '..', 'client/build');
-console.log(appPath);
 
 
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('client/build'));
+app.use(express.static(appPath));
 app.use(express.static('public'));
 
 app.set('json spaces', 4);
