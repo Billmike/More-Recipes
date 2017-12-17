d5019102b59c599825624a1ccb1f5f3fba209244
Bill Michael <billmike1994@gmail.com>
2017-12-17 15:34:10 +0100

    Modified webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,71 @@
 const path = require('path');
 
-module.exports = (env) => {
-  const isProduction = env === 'production';
-  return {
-    entry: './client/src/index.js',
-    output: {
-      path: path.join(__dirname, 'server/build'),
-      filename: 'bundle.js',
-    },
-    module: {
-      rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'client/public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      loaders: ['babel-loader?presets[]=react,presets[]=env,presets[]=stage-0'],
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+    }, {
+      test: /\.(png|jpg|jpeg|gif)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {},
+        },
       ],
-    },
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
-    devServer: {
-      contentBase: path.join(__dirname, 'build'),
-      historyApiFallback: true,
-    },
-  };
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+      ],
+    }],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'client/public'),
+    historyApiFallback: true,
+  },
 };
+
+// module.exports = (env) => {
+//   const isProduction = env === 'production';
+//   return {
+//     entry: './client/src/index.js',
+//     output: {
+//       path: path.join(__dirname, 'server/build'),
+//       filename: 'bundle.js',
+//     },
+//     module: {
+//       rules: [{
+//         loader: 'babel-loader',
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//       },
+//       {
+//         test: /\.s?css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader',
+//           'sass-loader',
+//         ],
+//       },
+//       ],
+//     },
+//     devtool: isProduction ? 'source-map' : 'inline-source-map',
+//     devServer: {
+//       contentBase: path.join(__dirname, 'build'),
+//       historyApiFallback: true,
+//     },
+//   };
+// };
 
