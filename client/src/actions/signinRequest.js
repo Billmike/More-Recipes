529f324593343bd47448de72efd7b14d43f58725
Ayelegun Kayode Michael <billmike1994@gmail.com>
2018-02-13 13:32:58 +0100

    prepare for final push

--- client/src/actions/signinRequest.js
+++ client/src/actions/signinRequest.js
@@ -1,52 +1,28 @@
 import axios from 'axios';
 import jwt from 'jsonwebtoken';
-import { SET_CURRENT_USER, GET_USER_INFORMATION } from './types';
+import { SET_CURRENT_USER } from './types';
 import setAuthToken from '../utils/setAuthToken';
 import './toastrConfig';
 
-export const setCurrentUser = (user) => {
-  return {
-    type: SET_CURRENT_USER,
-    user,
-  };
+export const setCurrentUser = user => ({
+  type: SET_CURRENT_USER,
+  user
+});
+
+export const signinRequest = userData => dispatch => axios
+  .post('/api/v1/users/signin', userData)
+  .then((res) => {
+    const { token } = res.data;
+    localStorage.setItem('authToken', token);
+    toastr.success('Login Successful.');
+    setAuthToken(token);
+    dispatch(setCurrentUser(jwt.decode(token)));
+  })
+  .catch(error => Promise.reject(error.response.data.message));
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('authToken');
+  toastr.success('Logout Successful.');
+  setAuthToken(false);
+  dispatch(setCurrentUser({}));
 };
-
-// export const getUserInformation = (user) => {
-//   return {
-//     type: GET_USER_INFORMATION,
-//     user,
-//   };
-// };
-
-export const signinRequest = userData => (dispatch) => {
-  return axios.post('http://localhost:8000/api/v1/users/signin', userData)
-    .then((res) => {
-      const { token } = res.data;
-      localStorage.setItem('authToken', token);
-      toastr.success('Login Successful.');
-      setAuthToken(token);
-      dispatch(setCurrentUser(jwt.decode(token)));
-    })
-    .catch(error => Promise.reject(error.response.data.message));
-};
-
-export const logout = () => {
-  return (dispatch) => {
-    localStorage.removeItem('authToken');
-    toastr.success('Logout Successful.');
-    setAuthToken(false);
-    dispatch(setCurrentUser({}));
-  };
-};
-
-// export const startGetUserInfo = () => {
-//   return (dispatch) => {
-//     axios.get('http://localhost:8000/api/v1/users/get_user')
-//       .then((res) => {
-//         console.log(res);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-// };
--- client/src/actions/userSignupAction.js
+++ client/src/actions/userSignupAction.js
@@ -3,21 +3,18 @@ import jwt from 'jsonwebtoken';
 import setAuthToken from '../utils/setAuthToken';
 import { SET_CURRENT_USER } from './types';
 
-export const setCurrentUser = (user) => {
-  return {
-    type: SET_CURRENT_USER,
-    user,
-  };
-};
+export const setCurrentUser = user => ({
+  type: SET_CURRENT_USER,
+  user
+});
 
-export const signupRequest = userData => (dispatch) => {
-  return axios.post('http://localhost:8000/api/v1/users/signup', userData)
-    .then((res) => {
-      const { token } = res.data;
-      localStorage.setItem('authToken', token);
-      toastr.success('Login Successful.');
-      setAuthToken(token);
-      dispatch(setCurrentUser(jwt.decode(token)));
-    })
-    .catch(error => Promise.reject(error.response.data.message));
-};
+export const signupRequest = userData => dispatch => axios
+  .post('/api/v1/users/signup', userData)
+  .then((res) => {
+    const { token } = res.data;
+    localStorage.setItem('authToken', token);
+    toastr.success('Login Successful.');
+    setAuthToken(token);
+    dispatch(setCurrentUser(jwt.decode(token)));
+  })
+  .catch(error => Promise.reject(error.response.data.message));
