338456e2a2102b28f4683e05421da49be59076b1
Ayelegun Kayode Michael <billmike1994@gmail.com>
2018-02-14 03:38:09 +0100

    Add footer'

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <div>
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  </div>
+);
+
+export default Footer;
